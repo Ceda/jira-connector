e75fe7785ebd7417ca4461ba047f327e4b8b6106
Martin Magnusek <magnusekm@gmail.com>
2016-08-25 09:39:06 +0200

    Rename app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "jira-transition-rails-5_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "jira-transition_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
